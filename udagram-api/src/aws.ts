3589a65807f50365f83030453a7675f9b897acac
Peter Edoja <edoja2012@gmail.com>
2022-06-13 12:11:09 +0100

    ft(): microservice implementation

--- udagram-api/src/aws.ts
+++ udagram-api/src/aws.ts
@@ -5,6 +5,7 @@ import {config} from './config/config';
 // Configure AWS
 const credentials = new AWS.SharedIniFileCredentials({profile: config.aws_profile});
 AWS.config.credentials = credentials;
+console.log(`credentials: ${JSON.stringify(credentials)}`)
 
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
