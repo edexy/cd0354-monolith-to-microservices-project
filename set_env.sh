25c9742132d0fd685b3222b00e3b8c0e5be1c178
Peter Edoja <edoja2012@gmail.com>
2022-06-14 20:02:44 +0100

    ft(): include screenshots

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=9FWZnFtherBZ3YLEnUz0
-export POSTGRES_HOST=udgram.cvkamhvatofl.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=notmypassword
+export POSTGRES_HOST=localhost
 export POSTGRES_DB=postgres
-export AWS_BUCKET=udgram-11169128894
-export AWS_REGION=us-east-1
+export AWS_BUCKET=notmybucket
+export AWS_REGION=us-east-2
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
